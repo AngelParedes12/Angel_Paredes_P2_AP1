0a49f15121df7bee69f2766bf5d3bf0804f4b7ef
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-03-10 18:43:26 -0400

    creacion de modelo ciudades

---
+++ Components/Models/Ciudades.cs
@@ -0,0 +1,9 @@
+﻿namespace Angel_Paredes_P2_AP1.Components.Models
+{
+    public class Ciudades
+    {
+        public int CiudadId { get; set; }
+        public string NombreCiudades { get; set; }
+        public int Monto { get; set; }
+    }
+}
