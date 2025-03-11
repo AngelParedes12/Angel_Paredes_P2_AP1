1b5e3c5bdaf524e898bf4700d4fcf4050e605ce9
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-03-10 20:40:27 -0400

    creando modelo detalle

---
+++ Components/Models/Detalle.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Angel_Paredes_P2_AP1.Components.Models;
+
+public class Detalle
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "La fecha es requerida")]
+    public DateTime Fecha { get; set; } = DateTime.Today;
+
+    [Required(ErrorMessage = "La asignatura es obligatoria")]
+    [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+    public string Asignatura { get; set; }
+
+    [Range(0.01, 1000000, ErrorMessage = "El monto debe ser mayor a 0")]
+    public decimal Monto { get; set; }
+
+    public int CiudadId { get; set; }
+}
