e90cd96cb3c020d98df2f9460bb2acf059c4fbe8
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-03-10 20:40:42 -0400

    modificacion de modelo vacio que tenia

--- Components/Models/Modelo.cs
+++ Components/Models/Modelo.cs
@@ -1,6 +1,41 @@
-﻿namespace Angel_Paredes_P2_AP1.Components.Models
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+public class Modelo
 {
-    public class Modelo
-    {
-    }
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Nombre es requerido")]
+    public string Nombre { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "Monto inválido")]
+    public decimal MontoTotal { get; set; }
+    public object Detalles { get; internal set; }
+}
+
+public class Detalle
+{
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime Fecha { get; set; } = DateTime.Today;
+
+    [Required(ErrorMessage = "Asignatura es requerida")]
+    public string Asignatura { get; set; }
+
+    [Range(0.01, 1000000, ErrorMessage = "Monto debe ser mayor a 0")]
+    public decimal Monto { get; set; }
+
+    [ForeignKey("Ciudad")]
+    public int CiudadId { get; set; }
+    public Ciudad Ciudad { get; set; }
+}
+
+public class Proyecto
+{
+    public int Id { get; set; }
+    public decimal Presupuesto { get; set; }
+    public int CiudadId { get; set; }
+    public Ciudad Ciudad { get; set; }
 }
