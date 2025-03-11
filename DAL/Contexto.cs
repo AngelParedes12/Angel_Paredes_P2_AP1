79a7b05066dc6b535506a2331587848ef73ff9ae
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-03-10 20:41:45 -0400

    configuracion de contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,6 +1,19 @@
-﻿namespace Angel_Paredes_P2_AP1.DAL
+﻿using Angel_Paredes_P2_AP1.Components.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppContext : DbContext
 {
-    public class Contexto
+    public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+    
+    public DbSet<Ciudad> Ciudades { get; set; }
+    public DbSet<Detalle> Detalles { get; set; }
+    public DbSet<Proyecto> Proyectos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Ciudad>()
+            .HasMany(c => c.Detalles)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
