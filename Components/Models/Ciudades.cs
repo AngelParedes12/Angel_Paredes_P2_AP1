8595e535612bb47e7e3f06c0cbe947d8147d21d0
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-03-10 20:40:09 -0400

    creando el modelo ciudad

--- Components/Models/Ciudades.cs
+++ Components/Models/Ciudades.cs
@@ -1,9 +1,20 @@
-﻿namespace Angel_Paredes_P2_AP1.Components.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+namespace Angel_Paredes_P2_AP1.Components.Models
 {
-    public class Ciudades
+
+    public class Ciudad
     {
-        public int CiudadId { get; set; }
-        public string NombreCiudades { get; set; }
-        public int Monto { get; set; }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Mínimo 3 caracteres")]
+        public string Nombre { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "El monto no puede ser negativo")]
+        public decimal MontoTotal { get; set; }
+
+        public List<Detalle> Detalles { get; set; } = new List<Detalle>();
+        public List<Proyecto> Proyectos { get; set; } = new List<Proyecto>();
     }
 }
